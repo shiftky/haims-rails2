bf38edb4465f2ca840d5fc0644ea3a87c8a73ec6
shiftky <shiftky@gmail.com>
2013-08-05 23:28:15 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaimsRails2::Application.config.secret_key_base = 'bd205ef58f1349159f5b637b06278f6cc6bf3dfd11302422a1b2685cb4fb3d5d92a73fb249deaadbb9eec497ddefa13c9b6c5dd77a9dd1f1ff5656813021c29c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HaimsRails2::Application.config.session_store :cookie_store, key: '_haims-rails2_session'
